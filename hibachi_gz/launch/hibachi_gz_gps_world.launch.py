474e431f478b784605126d4a90b1b3559dbd5f71
Guido Sanchez <sanchez.guido@gmail.com>
2024-07-02 15:53:02 +0000

    Add launcher for empty world with GPS plugin

---
+++ hibachi_gz/launch/hibachi_gz_gps_world.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+import math
+
+def generate_launch_description():
+    
+    world_file = PathJoinSubstitution(
+        [FindPackageShare("hibachi_gz"),
+        "worlds",
+        "empty_world.world"],
+    )
+
+    gz_launch = PathJoinSubstitution(
+        [FindPackageShare("hibachi_gz"),
+        "launch",
+        "hibachi_gz.launch.py"],
+    )
+
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([gz_launch]),
+        launch_arguments = {
+            'world_path': world_file,
+            'x': '0.0',
+            'y': '0.0',
+            # 'z': '0.11',
+            # 'yaw': str(0.0*math.pi/2.0),
+        }.items(),
+    )
+
+    ld = LaunchDescription([])
+    ld.add_action(gz_sim)
+
+    return ld
